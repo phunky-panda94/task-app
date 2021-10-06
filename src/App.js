857eabad29b06af65d09c6378bfdb3ee78f6c894
phunky-panda94 <victor.nim94@outlook.com>
2021-10-06 12:19:10 +1100

    implemented edit functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Overview from './components/Overview';
+import './Overview.css';
 
 class App extends Component {
     
@@ -14,10 +15,12 @@ class App extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
 
     }
 
     handleChange(event) {
+        console.log(event.target.value);
         this.setState({
             task: event.target.value
         })
@@ -29,6 +32,7 @@ class App extends Component {
             tasks: this.state.tasks.concat(this.state.task),
             task: ''
         });
+        event.target.reset();
     }
 
     handleDelete(index) {
@@ -36,15 +40,47 @@ class App extends Component {
             tasks: this.state.tasks.filter((task,id) => id !== index),
         });
     }
+
+    handleEdit(index) {
+
+        let taskInput = document.querySelectorAll(`li > input`)[index];
+        console.log(taskInput.value);
+        // update item in array
+        let list = this.state.tasks.map((task,id) => {
+            if (id === index) {
+                return taskInput.value;
+            } 
+            return task;
+        })
+
+        this.setState({
+            tasks: list
+        })
+
+    }
+
+    toggle(event, index) {
+
+        let taskInput = document.querySelectorAll(`li > input`)[index];
+
+        if (taskInput.disabled) {
+            taskInput.disabled = false;
+            event.target.textContent = 'Save';
+        } else {
+            taskInput.disabled = true;
+            event.target.textContent = 'Edit';
+        }
+
+    }
     
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.task} onChange={this.handleChange}></input>
+                    <input type="text" onChange={this.handleChange}></input>
                     <input type="submit"></input>
                 </form>
-                <Overview tasks={this.state.tasks} onDelete={this.handleDelete}/>
+                <Overview tasks={this.state.tasks} onDelete={this.handleDelete} onEdit={this.handleEdit} toggle={this.toggle} />
             </div>
         );
     }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../Overview.css';
 
 class Overview extends Component {
 
@@ -7,8 +8,9 @@ class Overview extends Component {
             <div>
                 <ul>
                     {this.props.tasks.map((task, index) => 
-                        <li key={index}>{task}
-                            <button>Edit</button>
+                        <li key={index}>
+                            <input type="text" value={task} onChange={() => this.props.onEdit(index)} disabled/>
+                            <button onClick={(e) => this.props.toggle(e,index)}>Edit</button>
                             <button onClick={() => this.props.onDelete(index)}>Delete</button>
                         </li>
                     )}
